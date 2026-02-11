d3577f41ea630232bf49e85beb9ac1b64a450e99
RaghulKumar-AI-ML <raghul.k.1004@gmail.com>
2026-02-11 21:26:59 +0530

    worked on extra numpy things

--- numpyexercises.py
+++ numpyexercises.py
@@ -4,7 +4,18 @@ print(arr)
 
 arr2=np.array([[2,3,4],
                [5,6,7]])
+"""
 print(arr2)
 print(arr2.shape)
 print(type(arr2))
 print(arr2.dtype)
+print(arr2.ndim)
+print(arr2.reshape(1,6))
+"""
+"""print(arr2[1,1])
+print(arr2[:,1])
+print(arr2[0,:])
+print(arr2[0:2,1:3])
+arr2[0,0]=20
+print(arr2)
+"""
